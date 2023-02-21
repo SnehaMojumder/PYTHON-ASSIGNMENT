ea9d7c48a3b2b99536e6ad8b4b43c95473b8e900
SnehaMojumder <sneha.majumder@wipro.com>
2023-02-21 10:41:35 +0530

    first commit

---
+++ Longest-Substring.py
@@ -0,0 +1,18 @@
+def longestBeautifulSubstring(word:str)->int:
+    vowels='aeiou'
+    longest=0
+    start=0
+    for i in range(len(word)):
+        if i>0 and word[i]<word[i-1]:
+            start=i
+        if word[i]==vowels[-1]:
+            if all(v in word[start:i+1] for v in vowels):
+                if i-start+1>longest:
+                    longest=i-start+1
+            else:
+                start=i
+    return longest
+def main():
+    word="aeeiiouu"
+    longest=longestBeautifulSubstring(word)
+    print(longest)
